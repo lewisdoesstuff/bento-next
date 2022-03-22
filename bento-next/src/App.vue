6e68d6218c340db7db67aff1f5302d45dc56b63a
Lewis Wood <lew@lew.ooo>
2022-03-22 23:19:42 +0000

    Vue JS initial init

---
+++ bento-next/src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
